78deb2f022e652de010c0e489cbb2de4bb25b18c
Cristian Lozano <criloz@nebtex.com>
2017-04-23 20:11:05 -0500

    observable queries

--- src/Taurus.ts
+++ src/Taurus.ts
@@ -1,17 +1,21 @@
-import { ObservableMap } from 'mobx';
+import { ObservableMap, observable } from 'mobx';
 import { ObservableHashMap } from '@nebtex/hashmaps';
-import { Minimatch, filter }from 'minimatch';
+import { Minimatch, filter } from 'minimatch';
+
+export class Result {
+  @observable value: any
+}
 
 export class ResourceMap<V> extends ObservableHashMap<Array<number>, V> {
-  constructor(){
+  constructor() {
     super();
   }
 
-  protected objectHash(key:any){
+  protected objectHash(key: any) {
     return key.join('/');
   }
 
-  filterByHash(pattern:string) {
+  filterByHash(pattern: string) {
     const filteredValues = this.internalMap.keys()
       .filter(filter(pattern))
       .map(key => this.internalMap.get(key));
@@ -23,16 +27,20 @@ export class ResourceMap<V> extends ObservableHashMap<Array<number>, V> {
 export class Offline<V>{
   applications: Map<string, ResourceMap<V>> = new Map();
 
-  register(app:string, hashMap:ResourceMap<V>) {
+  register(app: string, hashMap: ResourceMap<V>) {
     this.applications.set(app, hashMap);
   }
 
-  query(app:string, pattern:string, handler: (queryset:Array<V>) => void): void{
+  query(app: string, pattern: string, handler: (queryset: Array<V>) => void): any {
     const hashMap = this.applications.get(app);
-    if(!hashMap)
+    if (!hashMap)
       return void 0
-    
-    const queryset = hashMap.filterByHash(pattern);
-    handler(queryset);
+
+    let result = new Result()
+    let disposer = hashMap.observe(function () {
+      result.value = handler(hashMap.filterByHash(pattern))
+    })
+    result.value = handler(hashMap.filterByHash(pattern));
+    return { result, disposer }
   }
 }
--- src/__tests__/Taurus.test.ts
+++ src/__tests__/Taurus.test.ts
@@ -1,7 +1,7 @@
 import { Offline, ResourceMap } from '../Taurus';
 import { ObservableHashMap } from '@nebtex/hashmaps';
 import test from 'ava';
-
+import {autorun} from 'mobx';
 
 let hashMap: ResourceMap<any>;
 
@@ -10,14 +10,21 @@ test.beforeEach(t => {
 });
 
 test('mock test', t => {
-  hashMap.set([1,2,634], 'object1');
-  hashMap.set([1,2,6,67], 'object2');
-  hashMap.set([12,23,64], 'object3');
-  hashMap.set([1,2,6], 'object4');
+  hashMap.set([1, 2, 634], 1);
+  hashMap.set([1, 2, 6, 67], 2);
+  hashMap.set([12, 23, 64], 100);
+  hashMap.set([1, 2, 6], 3);
 
   let offline = new Offline();
   offline.register('io.omniql.v1', hashMap);
-  offline.query('io.omniql.v1', '1/2/*', function(queryset){
-    t.is(queryset.length, 2);
+  const { result, disposer } = offline.query('io.omniql.v1', '1/2/*', function (queryset) {
+    return queryset.reduce((x: number, y: number) => x + y);
   });
+
+  var disposer2 = autorun(() => console.log(result.value))
+
+  hashMap.set([1,2,7], 9);
+  hashMap.set([1,2,8], 27);
+  disposer2()
+  hashMap.set([1,2,7], 9);
 });
